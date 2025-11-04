1585fdd59748338ecba0761e908ae9ac94d777f5
saimeunt <saimeunt@gmail.com>
2025-11-04 10:10:31 +0100

    walnut config

--- services/server/src/config/walnut.js
+++ services/server/src/config/walnut.js
@@ -1,4 +1,8 @@
 module.exports = {
+  serverUrl:
+    process.env.NODE_ENV !== "production"
+      ? "http://sourcify.walnut.local"
+      : "https://sourcify.walnut.dev",
   corsAllowedOrigins: [
     process.env.NODE_ENV !== "production"
       ? "http://verify.walnut.local"
